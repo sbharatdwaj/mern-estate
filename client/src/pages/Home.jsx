4cd23554dde3256907f018d1370374c92764a99f
sbharatdwaj <shivambaratdwaj3@gmail.com>
2024-11-15 22:10:01 +0530

    init commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=" text-2xl font-extrabold text-blue-400 "> 
+	Home Me Aa Jao Kundi Mat Kahdkaao
+    </div>
+  )
+}
+
+export default Home
